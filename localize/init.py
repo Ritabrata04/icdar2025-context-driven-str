ba56e177e090ddc552314f7485345839b6f7fa21
Ritabrata Chakraborty <125497227+Ritabrata04@users.noreply.github.com>
2025-08-27 15:53:32 +0530

    Create init.py

---
+++ localize/init.py
@@ -0,0 +1,9 @@
+"""
+Public API for `localize` package.
+Exports:
+- BlockParam
+- mask_to_blocks
+"""
+from .mask_to_blocks import BlockParam, mask_to_blocks
+
+__all__ = ["BlockParam", "mask_to_blocks"]
